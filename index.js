a1312ef6847c0449fa6981bca3d6afeca777d4da
intsuc <intsuc@gmail.com>
2023-06-07 02:02:05 +0900

    Add dependency detector

--- index.js
+++ index.js
@@ -2,6 +2,40 @@
 
 "use strict";
 
-async function run() { }
+import * as core from "@actions/core";
+import { BuildTarget, PackageCache, Snapshot, submitSnapshot } from "@github/dependency-submission-toolkit";
+import { PackageURL } from "packageurl-js";
+import { readFile } from "fs/promises";
+
+async function run() {
+  try {
+    // TODO: install mcx
+
+    const pack = JSON.parse(await readFile("pack.json", "utf8"));
+    const cache = new PackageCache();
+    const target = new BuildTarget(pack.name);
+
+    for (const key in pack.dependencies) {
+      const triple = /^([^\/]+)\/([^@]+)@(.+)$/.exec(pack.dependencies[key]);
+      if (triple === null) {
+        throw new Error(`invalid dependency triple: ${pack.dependencies[key]}`);
+      }
+      const [_, owner, repository, tag] = triple;
+      const purl = new PackageURL("github", owner, repository, tag, null, null);
+      // TODO: indirect dependencies
+      target.addBuildDependency(cache.package(purl));
+    }
+
+    const snapshot = new Snapshot({
+      name: "setup-mcx",
+      url: "https://github.com/mcenv/setup-mcx",
+      version: "0.1.0",
+    });
+    snapshot.addManifest(target);
+    submitSnapshot(snapshot);
+  } catch (error) {
+    core.setFailed(`${error}`);
+  }
+}
 
 run();
